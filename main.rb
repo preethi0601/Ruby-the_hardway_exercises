f642ff9b7a5f825b594c398f6aa6919e47e80508
Prithi <preiths97@gmail.com>
2021-07-31 17:45:11 +0530

    ruby learning part1

---
+++ main.rb
@@ -0,0 +1,10 @@
+#Module 1 
+
+puts "Enter a number" #puts - put string (for printing output)
+#assign a variable 'test' to get the data - gets(get string | input )
+test = gets 
+
+puts "\n\n"  # '\n' is carriage return - printing out new link_to_remote
+
+puts "#{test} " # String interpolation to read the data of a variable.
+
