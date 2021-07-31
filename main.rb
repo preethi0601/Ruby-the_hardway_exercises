9029b371b271358a80565fe3be2791ef3cbb5791
Prithi <preiths97@gmail.com>
2021-07-31 18:12:04 +0530

    Arrays-Problems

--- main.rb
+++ main.rb
@@ -9,3 +9,30 @@ puts "\n\n"  # '\n' is carriage return - printing out new link_to_remote
 puts "#{test} " # String interpolation to read the data of a variable.
 
 #----Making changes for git commit 
+
+#------ARRAYS : Problem1
+names = [["Jhumpa", "Lahiri"], ["J. K", "Rowling"], ["Devdutt", "Pattanaik"]]
+new_names =[]
+new_names.push(names[0].join(" "))
+new_names.push(names[1].join(" "))
+new_names.push(names[2].join(" "))
+puts new_names
+
+#------ARRAYS : Problem2
+books = ["Design as Art", "Anathem", "Shogun"] 
+authors = ["Bruno Munari", "Neal Stephenson", "James Clavell"]
+puts books.map.with_index {|book,i| "#{book} is written by #{authors[i]}"}
+
+#------ARRAYS : Problem3
+#soln1
+todos = [["money", ["Send invoice", "Pay rent","Pay taxes"]], ["organize" ,["clean room","arrange books"]], ["food",["buy groceries"]]]
+
+query = todos.find {todos[0][0] =="money"}
+if query == nil 
+    puts "No such todo!"
+    else 
+        puts todos[0][1]
+    end
+ #soln2:
+  todos.each { |i|
+ puts i.join(" ")}
